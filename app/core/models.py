ac76273c53c2e40f79026048ece7efb8f7c445d0
Adam Kielar <adam@adamkielar.pl>
2020-08-03 18:09:01 +0200

    Added book model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,24 @@
+from django.contrib.postgres.fields import ArrayField
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    """Book object"""
+    title = models.CharField(max_length=255)
+    authors = ArrayField(models.CharField(max_length=255))
+    published_date = models.CharField(max_length=10, blank=True)
+    categories = ArrayField(
+        models.CharField(max_length=255),
+        blank=True,
+        null=True
+    )
+    average_rating = models.PositiveSmallIntegerField(
+        blank=True,
+        validators=[MaxValueValidator(5)]
+    )
+    ratings_count = models.PositiveIntegerField(blank=True)
+    thumbnail = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title

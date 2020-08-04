f14f6e17811f3d35ce11e8767f8c95e0c91fcb23
Adam Kielar <adam@adamkielar.pl>
2020-08-04 13:03:36 +0200

    Added book serializer

---
+++ app/book/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from core.models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    """Serializer for book object"""
+
+    class Meta:
+        model = Book
+        fields = (
+            'id',
+            'title',
+            'authors',
+            'published_date',
+            'categories',
+            'average_rating',
+            'ratings_count',
+            'thumbnail'
+        )
+        read_only_fields = ('id')

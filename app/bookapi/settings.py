314ddeb6042e6b4a5e5096f559888fc50bd4f63b
Adam Kielar <adam@adamkielar.pl>
2020-08-05 20:32:03 +0200

    Removed django-filters module

--- app/bookapi/settings.py
+++ app/bookapi/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_filters',
     'rest_framework',
     'core',
     'book',
